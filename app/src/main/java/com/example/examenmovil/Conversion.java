8932930a4349134968e88e0311ab812d5726c6c2
JohnPaulVal <49104361+JohnPaulVal@users.noreply.github.com>
2019-05-22 01:09:01 -0400

    Ya me falta poco :"3

---
+++ app/src/main/java/com/example/examenmovil/Conversion.java
@@ -0,0 +1,84 @@
+package com.example.examenmovil;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Conversion extends AppCompatActivity {
+    public String tp="",c;
+    EditText n,o;
+    TextView r;
+    public double num,tot;
+    private EditText et1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversion);
+        tp = getIntent().getStringExtra("tp");
+        c = getIntent().getStringExtra("c");
+        et1=(EditText)findViewById(R.id.et1);
+        o=(EditText)findViewById(R.id.tx1);
+        SharedPreferences prefe=getSharedPreferences("datos", Context.MODE_PRIVATE);
+        et1.setText(prefe.getString("conversion",""));
+        this.setTitle(tp);
+    }
+    public void Calcular(View v){
+        try{
+            r=(TextView)findViewById(R.id.tvt);
+            n=(EditText)findViewById(R.id.tx1);
+            num=Integer.parseInt(n.getText().toString());
+            switch (c){
+                case "1":
+                    tot=(num*9/5)+32;
+                    r.setText(tot+" F°");
+                    break;
+                case "2":
+                    tot=(num-32)*5/9;
+                    r.setText(tot+" C°");
+                    break;
+                case "3":
+                    tot=num+273.15;
+                    r.setText(tot+" K°");
+                    break;
+                case "4":
+                    tot=num-273.15;
+                    r.setText(tot+" C°");
+                    break;
+                case "5":
+                    tot=num*10000;
+                    r.setText(tot+" m²");
+                    break;
+                case "6":
+                    tot=num/10000;
+                    r.setText(tot+" ha");
+                    break;
+                case "7":
+                    tot=num/1000;
+                    r.setText(tot+" km");
+                    break;
+                case "8":
+                    tot=num*1000;
+                    r.setText(tot+" m²");
+
+                    break;
+            }
+
+        }
+        catch (Exception e){
+            Toast.makeText(getApplicationContext(),"INGRESE UN NUMERO ",Toast.LENGTH_SHORT).show();
+        }
+        SharedPreferences preferencias=getSharedPreferences("datos",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=preferencias.edit();
+        editor.putString("conversion", o.getText().toString());
+        editor.commit();
+
+
+        //finish();
+    }
+
+}
--- app/src/main/java/com/example/examenmovil/MainActivity.java
+++ app/src/main/java/com/example/examenmovil/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.examenmovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public String tp="",c="";
+    public Button cf,fc,ck,kc,hm,mh,mk,km;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void uno(View v){
+        cf=(Button)findViewById(R.id.bt1);
+        tp=cf.getText().toString();
+        c="1";
+        CambioPantalla();
+    }
+    public void dos(View v){
+        fc=(Button)findViewById(R.id.bt2);
+        tp=fc.getText().toString();
+        c="2";
+        CambioPantalla();
+    }
+    public void tres(View v){
+        ck=(Button)findViewById(R.id.bt3);
+        tp=ck.getText().toString();
+        c="3";
+        CambioPantalla();
+    }
+    public void cuatro(View v){
+        kc=(Button)findViewById(R.id.bt4);
+        tp=kc.getText().toString();
+        c="4";
+        CambioPantalla();
+    }
+    public void cinco(View v){
+        hm=(Button)findViewById(R.id.btn5);
+        tp=hm.getText().toString();
+        c="5";
+        CambioPantalla();
+    }
+    public void seis(View v){
+        mh=(Button)findViewById(R.id.bt6);
+        tp=mh.getText().toString();
+        c="6";
+        CambioPantalla();
+    }
+    public void siete(View v){
+        mk=(Button)findViewById(R.id.btn7);
+        tp=mk.getText().toString();
+        c="7";
+        CambioPantalla();
+    }
+    public void ocho(View v){
+        km=(Button)findViewById(R.id.btn8);
+        tp=km.getText().toString();
+        c="8";
+        CambioPantalla();
+    }
+    public void CambioPantalla(){
+        Intent Ventana=new Intent(getApplicationContext(),Conversion.class);
+        Ventana.putExtra("tp",tp);
+        Ventana.putExtra("c",c);
+        startActivity(Ventana);
+    }
+
+
 }
